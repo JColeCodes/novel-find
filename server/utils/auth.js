6d268475f4f0d9fe352d2786481ade13f4b18d15
Jennifer Cole <capauldi@gmail.com>
2022-04-03 07:40:18 -0700

    code cleanup

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function ({ req }) {
+  authMiddleware: function ({ req, res }) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -16,7 +16,6 @@ module.exports = {
     }
 
     if (!token) {
-      // return res.status(400).json({ message: 'You have no token!' });
       return req;
     }
 
@@ -26,11 +25,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return res.status(400).json({ message: 'Invalid token!' });
     }
 
     // send to next endpoint
-    // next();
     return req;
   },
   signToken: function ({ username, email, _id }) {
