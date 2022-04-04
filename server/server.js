23930fe71948d9a8b5f0f129e91f35b4046bd738
Jennifer Cole <capauldi@gmail.com>
2022-04-03 17:37:23 -0700

    added wildcard for pages that don't exist

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
